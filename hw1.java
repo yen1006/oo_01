eb94b00337ba3bd73f581a98a4040c097ab777bf
yen1006 <55127506+yen1006@users.noreply.github.com>
2019-11-19 16:10:58 +0800

    Create hw1.java

---
+++ hw1.java
@@ -0,0 +1,27 @@
+abstract class Cshpe{
+  protected String color;
+  public void setColor(Sting str){
+    color=str;
+  }
+  public abstract void show();
+ }
+  
+class CTriangle extends CSharp{
+  protected Double aline,bline,cline;
+  public CTriangle(double a,double b,double c){
+    aline=a;
+    bline=b;
+    cline=c;
+  }
+  public void show(){
+    System.out.println("area="+0.5*a*b);
+  }
+}
+public class hw1
+{
+  public static void main(String args[]){
+    CTraingle ctra=new CTraingle(3,4,5);
+    ctra.setColor("Red");
+      ctra.show();
+  }
+}

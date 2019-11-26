7b5d7756aaa15c835f340a07ada0c74d9e9a6ff4
yen1006 <55127506+yen1006@users.noreply.github.com>
2019-11-26 15:49:16 +0800

    Update 1126.java

--- 1126.java
+++ 1126.java
@@ -1,16 +1,38 @@
 interface Shape{
-  abstract void getArea(Double dou);
+  abstract Double getArea();
 }
 class Rectangle implements Shape{
   double length;
   double width;
-  public void toString(String str){
+  public Rectangle(double len,double wid){
+    length =len;
+    width=wid;
+  }
+ public String toString(){
+
+ } 
+  public Double getArea(){
+     System.out.println( "area=" + length*width);
   }
 }
+
 class Triangle implements Shape{
   double base;
   double height;
-  public void toString(String str){
+  
+  public String toString(){
     
+ }  
+  public Double getArea(){
+    System.out.println( "area=" + base*height/2);
   }
+}
+public class app11_7{
+  public static void main(String args[]){
+    Rectangle rec;
+    rec=new Rectangle(4,5);
+    Triangle tri;
+    tri=new Triangle(4,5);
+    rec.getArea();
+    tri.getArea();
 }

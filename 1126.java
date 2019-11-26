b55a6279967412afdc5c7584fce93cc3adea5aa8
yen1006 <55127506+yen1006@users.noreply.github.com>
2019-11-26 15:20:54 +0800

    Create 1126.java

---
+++ 1126.java
@@ -0,0 +1,16 @@
+interface Shape{
+  abstract void getArea(Double dou);
+}
+class Rectangle implements Shape{
+  double length;
+  double width;
+  public void toString(String str){
+  }
+}
+class Triangle implements Shape{
+  double base;
+  double height;
+  public void toString(String str){
+    
+  }
+}
